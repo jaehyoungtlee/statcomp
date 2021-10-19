0530d8b22730dc92035b9db5dff9961f98334946
Tim Lee <jaehyoungtlee@gmail.com>
2021-10-19 00:20:32 -0400

    calculate CI

---
+++ R/calculate_CI.R
@@ -0,0 +1,23 @@
+#' Confidence interval
+#'
+#' @details Takes as input a vector of data of length N and alpha and calculates a confidence interval for the estimate of the mean in the populaiton.
+#' @param x numeric, data of length N
+#' @param conf numeric, alpha
+#'
+#' @return confidence interval
+#' @import
+#' @export
+#'
+#' @examples
+#' x <- rnorm(1:10)
+#' calculate_CI(x, 0.9)
+#'
+calculate_CI <- function(x, conf = 0.95) {
+        stopifnot(is.vector(x) & is.numeric(x)) # x must be a numeric vector
+        N <- length(x)
+        alpha <- 1 - conf
+        degrees_freedom <- N - 1
+        t_score <- qt(p = alpha/2, df = degrees_freedom, lower.tail = FALSE)
+        se <- sd(x)/sqrt(N)
+        return(c((mean(x) - t_score * se), (mean(x) + t_score * se)))
+}
